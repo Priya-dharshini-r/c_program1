175150b0f5404784bf7a77b09fc8dc0b51fb9d91
Priyadharshini .R <58815997+Priyadharshini-R-105@users.noreply.github.com>
2020-08-01 21:33:28 +0530

    Create Middle charcters.c

---
+++ c/Strings/Middle charcters.c
@@ -0,0 +1,14 @@
+//Accept n number of string and print middle character
+
+//3 hello orange watermelon output: l an rm
+//odd middle
+//even middle two
+#include<stdio.h>
+int main()
+{
+int n;
+scanf("%d",&n);
+char str[n][1001];
+for(int i=0;i<=n;i+++)
+{
+scanf(
